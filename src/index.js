f2aaa71aa6132e8218d0dcf8307e2181822fe942
Rioba Ian <riobafelix@gmail.com>
2023-09-10 15:04:49 +0300

    change api url from api/ to api

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 
 const apiRoutes = require("./routes");
 
-app.use("/api/", apiRoutes);
+app.use("/api", apiRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
